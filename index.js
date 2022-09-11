42669781f7ae7e481f33b8dfa04671b47f5ec3ac
Arthur Benware <artpolitic91@gmail.com>
2022-09-11 17:25:35 -0400

    commit tree 8961a4a3e85383a451ce52975869a125f4aa3530

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// code your solution here
+function saturdayFun(activity = 'roller-skate') {
+   return `This Saturday, I want to ${activity}!`
+}
+function mondayWork(thing = 'go to the office') {
+   return `This Monday, I will ${thing}.`
+}
+function wrapAdjective(_string = '*') { 
+   return function(adj = 'special') {
+      if (_string==='*') {
+         return `You are *${adj}*!`
+      }
+      else return `You are ||${adj}||!`
+   }
+}
